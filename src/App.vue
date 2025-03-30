b3418e032d284b2a2eb2a6b5746685a56004b4e3
xavierbeaunol <xavier.beaunol001@gmail.com>
2025-03-30 11:02:51 +0200

    fix: ts warnings

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 <script setup lang="ts">
 //import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+//import { useRoute } from 'vue-router';
 
 //import CategoriesBar from './components/CategoriesBar.vue';
 
