b2a1bd16fd2c214d4ed0f6f4496015fb6fa4d791
xavierbeaunol <xavier.beaunol001@gmail.com>
2025-03-30 13:42:46 +0200

    offset categorie bar mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -146,7 +146,7 @@ nav.navbar {
   position: fixed;
   margin-top: 25px;
   z-index: 100;
-  margin-left: -10px;
+  margin-left: 100px;
   background-color: rgb(255, 255, 255);
   padding-top: 10px;
   box-shadow: 0 2px 4px rgba(0,0,0,0.05);
