0ca599d9b492781b34dc925d88baf64da61e065a
xavierbeaunol <xavier.beaunol001@gmail.com>
2025-03-30 11:27:01 +0200

    fix: api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5013/api', // ou ton backend hébergé
+  baseURL: 'https://kreyolkwest-backend.onrender.com/api', // ou ton backend hébergé
 });
+//https://kreyolkwest-backend.onrender.com
 
+//baseURL: 'http://localhost:5013/api',
 export default api;
