a6997d145e717ffb87524605c3c2217a7ab1765f
xavierbeaunol <xavier.beaunol001@gmail.com>
2025-04-01 16:27:31 +0200

    admin verification

--- src/views/Connexion.vue
+++ src/views/Connexion.vue
@@ -80,11 +80,11 @@
     localStorage.setItem('token', res.data.token)
     localStorage.setItem('admin', res.data.admin) // ← important
 
-    if (res.data.admin === 'oui') {
+    //if (res.data.admin === 'oui') {
       router.push('/admin') // 🎯 Redirection admin
-    } else {
-      router.push('/compte') // ✅ Redirection normale
-    }
+   // } else {
+    //  router.push('/compte') // ✅ Redirection normale
+  //  }
   } catch (err) {
     erreur.value = err.response?.data?.message || 'Erreur lors de la connexion.'
   }
