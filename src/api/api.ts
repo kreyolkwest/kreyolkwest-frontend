23406049f0ac3ee3254c2656c7389d7ae9299fdb
xavierbeaunol <xavier.beaunol001@gmail.com>
2025-04-01 15:39:49 +0200

    admin verif

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,15 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // ou ton backend hébergé
 });
-//https://kreyolkwest-backend.onrender.com
 
-//baseURL: 'http://localhost:5013/api',
+// Intercepteur pour ajouter le token automatiquement
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+
 export default api;
